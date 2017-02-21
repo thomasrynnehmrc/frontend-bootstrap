49404596694160fbd05c7e95c5f67133c98caf0e
iteratoruk <iteratoruk@gmail.com>
2017-02-21 10:51:57 +0000

    TXMNT-626 upgrade to Play 2.5.12

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -35,9 +35,9 @@ object Dependencies {
 
   val compile = Seq(
     filters,
-    "uk.gov.hmrc" %% "crypto" % "3.1.0",
+    "uk.gov.hmrc" %% "crypto" % "4.2.0",
     "uk.gov.hmrc" %% "play-filters" % "5.9.0",
-    "uk.gov.hmrc" %% "play-graphite" % "3.1.0",
+    "uk.gov.hmrc" %% "play-graphite" % "3.2.0",
     "com.typesafe.play" %% "play" % PlayVersion.current,
     "de.threedimensions" %% "metrics-play" % "2.5.13",
     "ch.qos.logback" % "logback-core" % "1.1.7"
