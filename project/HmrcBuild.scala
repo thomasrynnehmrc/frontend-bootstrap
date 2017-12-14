1182689216b26000d7bd33c7d295484f811e29be
Jakob Grunig <jakob.grunig@digital.hmrc.gov.uk>
2017-12-14 13:12:10 +0000

    PLATOPS-1164 - upgraded play-auditing

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -36,7 +36,7 @@ object Dependencies {
   val compile = Seq(
     filters,
     "uk.gov.hmrc" %% "crypto" % "4.4.0",
-    "uk.gov.hmrc" %% "play-auditing" % "3.2.1",
+    "uk.gov.hmrc" %% "play-auditing" % "3.3.0",
     "uk.gov.hmrc" %% "http-verbs" % "7.1.0",
     "uk.gov.hmrc" %% "http-verbs-play-25" % "0.9.0",
     "uk.gov.hmrc" %% "play-graphite" % "3.6.1",
