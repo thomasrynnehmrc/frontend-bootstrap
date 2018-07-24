8e08fad03fcab180355e634b170556d69787c716
Gordon McMullan <gordon.mcmullan@digital.hmrc.gov.uk>
2018-07-24 11:05:53 +0100

    Bump play-ui for EMACDEL-3797

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -45,7 +45,7 @@ object Dependencies {
     "uk.gov.hmrc"        %% "govuk-template"           % "5.3.0",
     "uk.gov.hmrc"        %% "play-config"              % "4.3.0",
     "uk.gov.hmrc"        %% "play-health"              % "2.1.0",
-    "uk.gov.hmrc"        %% "play-ui"                  % "7.17.0",
+    "uk.gov.hmrc"        %% "play-ui"                  % "7.18.0",
     "com.typesafe.play"  %% "play"                     % PlayVersion.current,
     "de.threedimensions" %% "metrics-play"             % "2.5.13"
   )
